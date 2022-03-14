3501859649d8ce1bb9e541b1f09b85f9ee468883
Shotaro Yamazaki <shotaro.ibramessi@gmail.com>
2022-03-14 16:58:10 +0900

    fix

--- shirokuma_student_v1.sol
+++ shirokuma_student_v1.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract ShirokumaStudentV1 is ERC721Enumerable, ReentrancyGuard, Ownable {
+contract ShirokumaStudentV1 is ERC721Enumerable, Ownable {
 
     uint256 _maxTotal;
     uint256 _money;
@@ -35,7 +34,7 @@ contract ShirokumaStudentV1 is ERC721Enumerable, ReentrancyGuard, Ownable {
         string memory st1 = getColor(shirokuma.owner, 'st1');
         string memory st2 = getColor(shirokuma.owner, 'st2');
         parts[0] = string(abi.encodePacked('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="500" height="500"><path d="M0 0 L 500 0 L 500 500 L 0 500" fill="', bg, '"/>'));
-        parts[1] = string(abi.encodePacked('<text x="380" y="470" fill="white" font-family="cursive" font-size="60">Lv', Utils.getLevel(shirokuma.exp), '</text>'));
+        parts[1] = string(abi.encodePacked('<text x="380" y="470" fill="white" font-family="cursive" font-size="60">Lv', Utils.toString(Utils.getLevel(shirokuma.exp)), '</text>'));
         parts[2] = string(abi.encodePacked('<path fill="#FFFFFF" stroke="', st0, '" stroke-width="5" stroke-miterlimit="10" d="M351.43,324.24c-17.52-23.48-38.3-38.27-49.23-35.81H193.45c-10.59-3.78-32.2,11.22-50.32,35.5 c-19.46,26.08-27.51,52.99-17.98,60.1c9.53,7.11,33.03-8.27,52.49-34.35c4.46-5.97,8.31-11.99,11.49-17.8v61.12 c-0.19,3.13-0.29,6.33-0.29,9.61c0,32.54,9.64,58.92,21.53,58.92s21.53-26.38,21.53-58.92c0-0.94-0.01-1.87-0.03-2.8h31.82 c-0.03,1.36-0.06,2.73-0.06,4.11c0,32.54,9.64,58.92,21.53,58.92s21.53-26.38,21.53-58.92c0-6.43-0.38-12.61-1.08-18.4v-52.94 c3.14,5.69,6.92,11.57,11.28,17.41c19.46,26.08,42.96,41.46,52.49,34.35S370.89,350.32,351.43,324.24z"/>'));
         parts[3] = string(abi.encodePacked('<ellipse fill="#FFFFFF" stroke="', st0, '" stroke-width="5" stroke-miterlimit="10" cx="252" cy="196" rx="151.28" ry="124.85"/>'));
         parts[4] = string(abi.encodePacked('<circle fill="', st1, '" cx="304.67" cy="170.16" r="10.24"/><circle fill="', st1, '" cx="190.89" cy="171.73" r="10.24"/><circle fill="', st1, '" cx="247.94" cy="201.35" r="9.83"/>'));
@@ -52,20 +51,19 @@ contract ShirokumaStudentV1 is ERC721Enumerable, ReentrancyGuard, Ownable {
     
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
         Shirokuma memory shirokuma = _shirokuma[tokenId];
-        
         string memory output = _getShirokumaSvg(shirokuma);
-        
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": shirokuma.name, "description": "', shirokuma.description, '", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "', shirokuma.name, '", "description": "', shirokuma.description, '", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
         output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
     }
 
-    function claim(string memory name, string memory description) public nonReentrant onlyOwner {
-        require(_money <= _token.allowance(msg.sender, address(this)) && totalSupply() < _maxTotal);
+    function claim(string memory name, string memory description) public {
+        uint256 total = totalSupply();
+        require(_money <= _token.allowance(msg.sender, address(this)) && total < _maxTotal && balanceOf(msg.sender) == 0);
         _token.transferFrom(msg.sender, address(this), _money);
         _addShirokuma(name, msg.sender, description);
-        _safeMint(_msgSender(), totalSupply());
+        _safeMint(_msgSender(), total);
     }
 
     function _addShirokuma(string memory name, address owner, string memory description) internal {
@@ -83,11 +81,15 @@ contract ShirokumaStudentV1 is ERC721Enumerable, ReentrancyGuard, Ownable {
         return _shirokuma[tokenId];
     }
 
-    function addExp(uint256 tokenId, uint256 exp) public payable onlyOwner {
+    function sendOwner() public onlyOwner {
+        _token.transfer(owner(), _token.balanceOf(address(this)));
+    }
+
+    function addExp(uint256 tokenId, uint256 exp) public onlyOwner {
         _shirokuma[tokenId].exp += exp;
     }
 
-    function setSupply(uint256 money, uint256 maxTotal) public payable onlyOwner {
+    function setSupply(uint256 money, uint256 maxTotal) public onlyOwner {
         _money = money;
         _maxTotal = maxTotal;
     }
