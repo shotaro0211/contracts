fba9c803934d15abdc77c5f1c2e80d03c1570aee
Shotaro Yamazaki <shotaro.ibramessi@gmail.com>
2022-04-13 01:42:40 +0900

    add next nft_minority_vote

--- nft_minority_vote.sol
+++ nft_minority_vote.sol
@@ -6,87 +6,137 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract MinorityVote is ERC721Enumerable, ReentrancyGuard, Ownable {
+    enum Answer {
+        Yes,
+        No,
+        Null
+    }
+
+    struct Nft {
+        uint256 stage;
+        uint256 game;
+        bool burn;
+        bool winner;
+    }
+    Nft[] private _nfts;
+
     struct Vote {
         uint256 tokenId;
-        bool answer;
-        uint256 questionId;
+        Answer answer;
+        uint256 questionIndex;
     }
     Vote[] private _votes;
 
     struct Question {
         string title;
+        uint256 stage;
+        uint256 game;
     }
     Question[] private _questions;
 
-    uint256 private _stage;
-    uint256 private _mintId = 1;
+    uint256 private _currentQuestionIndex;
+    uint256 private _currentGame;
+    uint256 private _currentGameStartMintId;
+    uint256 private _currentStage;
+    uint256 private _nextMintId;
 
-    function createQuestion(string memory title) public nonReentrant onlyOwner {
-        _questions.push(Question(title));
+    function _createQuestion(string memory title) internal {
+        _questions.push(Question(title, _currentStage, _currentGame));
+        _currentQuestionIndex += 1;
     }
 
-    function createVotes(Vote[] memory votes) public nonReentrant onlyOwner {
-        for(uint256 i = 0; i < votes.length; i++) { 
-            ownerOf(votes[i].tokenId);
-            _questions[votes[i].questionId];
-            _votes.push(votes[i]);
+    function _createVotes(Answer[] memory answers) internal {
+        require(answers.length == _nextMintId - _currentGameStartMintId, "length invalid");
+        for(uint256 i = 0; i < answers.length; i++) { 
+            _votes.push(Vote(_currentGameStartMintId + i, answers[i], _currentQuestionIndex));
         }
     }
 
-    function execution() public nonReentrant onlyOwner {
-        uint256 yes = 0;
-        uint256 no = 0;
-        uint256 questionId = _stage - 1;
-        bool win;
+    function nextQuestion(string memory title, Answer[] memory answers) public nonReentrant onlyOwner {
+        uint256 totalYes = 0;
+        uint256 totalNo = 0;
+        Answer win;
+
+        _createVotes(answers);
 
         for(uint256 i = 0; i < _votes.length; i++) {
-            if (_votes[i].questionId == questionId) {
-                if (_votes[i].answer == true) {
-                    yes += 1;
-                } else {
-                    no += 1;
+            if (_votes[i].questionIndex == _currentQuestionIndex) {
+                if (_votes[i].answer == Answer.Yes) {
+                    totalYes += 1;
+                } else if (_votes[i].answer == Answer.No) {
+                    totalNo += 1;
+                } else if (_votes[i].answer == Answer.Null) {
+                    _nfts[_votes[i].tokenId - 1].burn = true; 
                 }
             }
         }
-        if (yes != no) {
-            if (yes < no) {
-                win = true;
-            } else if (no < yes) {
-                win = false;
+        if (totalYes != totalNo) {
+            if (totalYes < totalNo) {
+                win = Answer.Yes;
+            } else if (totalNo < totalYes) {
+                win = Answer.No;
             }
             for(uint256 i = 0; i < _votes.length; i++) {
-                if (_votes[i].questionId == questionId && _votes[i].answer != win) {
-                    _burn(_votes[i].tokenId); 
+                if (_votes[i].questionIndex == _currentQuestionIndex && _votes[i].answer != win) {
+                    _nfts[_votes[i].tokenId - 1].burn = true; 
                 }
             }
         }
-        _stage += 1;
+        if (totalYes < 2 || totalNo < 2) {
+            for(uint256 i = 0; i < _votes.length; i++) {
+                if(_nfts[_votes[i].tokenId - 1].burn == false) {
+                    _nfts[_votes[i].tokenId - 1].winner = true;
+                }
+            }
+            _currentGame += 1;
+            _currentGameStartMintId = _nextMintId;
+            _currentStage = 1;
+        } else {
+            _currentStage += 1;
+        }
+        _createQuestion(title);
     }
 
-    function getQuestions() public view returns (Question[] memory) {
-        return _questions;
+    function getQuestion(uint256 index) public view returns (Question memory) {
+        return _questions[index];
     }
 
-    function getStage() public view returns (uint256) {
-        return _stage;
+    function getNft(uint256 tokenId) public view returns (Nft memory) {
+        uint256 index = tokenId -1;
+        return _nfts[index];
     }
 
-    function getVotes() public view returns (Vote[] memory) {
-        return _votes;
+    function getCurrentQuestion() public view returns (Question memory) {
+        return _questions[_currentQuestionIndex];
     }
 
-    function mint() public nonReentrant {
-        require(_mintId < 23, "Token ID invalid");
-        require(balanceOf(msg.sender) == 0, "Already mint invalid");
-        require(_stage == 1, "Already start invalid");
-        _safeMint(msg.sender, _mintId);
-        _mintId += 1;
+    function getVote(uint256 index) public view returns (Vote memory) {
+        return _votes[index];
+    }
+
+    function mint() public nonReentrant onlyOwner {
+        require(_currentStage == 1, "Already start invalid");
+        _nfts.push(Nft(1, _currentGame, false, false));
+        _safeMint(msg.sender, _nextMintId);
+        _nextMintId += 1;
     }
 
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "NFT Minority Vote #', toString(tokenId), '", "description": "", "image": "https://dentou-s3.s3.ap-northeast-1.amazonaws.com/NFT/nft_minority_vote/IMG_1249.png"}'))));
+        Nft memory nft = getNft(tokenId);
+        string memory imageName = _getImageName(nft);
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "NFT Minority Vote #', toString(tokenId), '", "description": "", "image": "https://dentou-s3.s3.ap-northeast-1.amazonaws.com/NFT/nft_minority_vote/', imageName, '.png"}'))));
         string memory output = string(abi.encodePacked('data:application/json;base64,', json));
         return output;
+    }
+
+    function _getImageName(Nft memory nft) internal pure returns (string memory) {
+        if (nft.burn == true) {
+            return "burn";
+        } else if (nft.winner == true) {
+            return "winner";
+        } else {
+            return toString(nft.stage);
+        }
     }
 
     function toString(uint256 value) internal pure returns (string memory) {
@@ -111,8 +161,13 @@ contract MinorityVote is ERC721Enumerable, ReentrancyGuard, Ownable {
         return string(buffer);
     }
 
-    constructor() ERC721("NFT Minority Vote", "NMV") Ownable() {
-        _stage = 1;
+    constructor(string memory title) ERC721("NFT Minority Vote", "NMV") Ownable() {
+        _currentGame = 1;
+        _currentGameStartMintId = 1;
+        _currentStage = 1;
+        _questions.push(Question(title, 1, 1));
+
+        _nextMintId = 1;   
     }
 }
 
